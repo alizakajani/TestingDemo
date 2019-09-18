ccee43123fda87688ddca3b54feeb1baafb25ee5
Aliza Kajani <aliza.kajani@gmail.com>
2019-09-18 13:09:22 -0400

    tabs fixed

--- test_calculate.py
+++ test_calculate.py
@@ -2,24 +2,24 @@ import pytest
 
 # continuous integration - test code everytime something new is added w/ Travis-CI
 @pytest.mark.parametrize("a, b, expected", [
-	(2, 3, 5),
-	(3, 7, 10),
-	(-2, 5, 3),
-	(2, 10, 12),
-	(0.1, 0.2, 0.3)
+    (2, 3, 5),
+    (3, 7, 10),
+    (-2, 5, 3),
+    (2, 10, 12),
+    (0.1, 0.2, 0.3)
 	])
 def test_add(a, b, expected):
-	from calculate import add
-	result = add(a, b)
-	assert result == pytest.approx(expected)
+    from calculate import add
+    result = add(a, b)
+    assert result == pytest.approx(expected)
 
 @pytest.mark.parametrize("a, b, expected", [
-	(2, 3, -1),
-	(7, 3, 4),
-	(-2, 5, -7),
-	(10, 2, 8)
-	])
+    (2, 3, -1),
+    (7, 3, 4),
+    (-2, 5, -7),
+    (10, 2, 8)
+    ])
 def test_subtract(a, b, expected):
-	from calculate import subtract
-	result = subtract(a, b)
-	assert result == expected
+    from calculate import subtract
+    result = subtract(a, b)
+    assert result == expected
